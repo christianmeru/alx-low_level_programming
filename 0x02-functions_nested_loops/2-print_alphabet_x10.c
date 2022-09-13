7b3b527cfd4181406e42a04f14f1bc71143d3b4c
Okoyo Meru CHristian <okoyomeru@gmail.com>
2022-09-13 13:08:01 -0700

    fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,10 +7,12 @@
 void print_alphabet_x10(void)
 {
 		int j;
-		for (j = 'a'; j <= 'z'; j++)
+
+		for (j = 0; j <= 9; j++)
 		{
 			int i;
-			for (i = 0; i <= 9; i++)
+
+			for (i = 'a'; i <= 'z'; i++)
 			{
 				_putchar(i);
 			}
