5f0e1426d1c47e3b808ded88e4f7e450d4486a94
Okoyo Meru CHristian <okoyomeru@gmail.com>
2022-09-13 20:26:04 -0700

    addition function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* add - computes additionof two imputs
+* Return: o when function executes
+*/
+
+int add(int, int)
+{
+	int r;
+
+	r = a + b;
+	_putchar(r);
+	return (0);
+}
