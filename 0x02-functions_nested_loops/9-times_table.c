f532b14cc4b4445098c6ff244447523207f8c52a
Okoyo Meru CHristian <okoyomeru@gmail.com>
2022-09-13 20:11:42 -0700

    print multiplication table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* times_table - prints multiplication of two numbers
+*
+*/
+
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			return (i * j);
+		}
+	}
+}
