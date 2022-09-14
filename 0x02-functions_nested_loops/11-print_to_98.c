f382251c3846fb1223ddad4118c25621038accd3
Okoyo Meru CHristian <okoyomeru@gmail.com>
2022-09-13 20:46:09 -0700

    display naturalnumber and ends at 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_to_98 - print all natural number and stops at 98
+* @n: argument
+* Return: always 0
+*/
+
+void print_to_98(int n)
+{
+	for (i = n, i <= 98; i++)
+	{
+		_putchar(i);
+	}
+	for (i = n; i >= 98; i--)
+	{
+		_putchar(i);
+	}
+}
