d0389c28b7dbd6f052cb271cea4c5c1cd836cef3
Okoyo Meru CHristian <okoyomeru@gmail.com>
2022-09-13 20:49:09 -0700

    hot fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,12 +8,14 @@
 
 void print_to_98(int n)
 {
+	int i;
+
 	for (i = n, i <= 98; i++)
 	{
-		_putchar(i);
+		_putchar(i + ',' + ' ');
 	}
 	for (i = n; i >= 98; i--)
 	{
-		_putchar(i);
+		_putchar(i + ',' + " ");
 	}
 }
